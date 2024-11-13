4338d3e3f73aab2c5e835558d5ae45312e34ace7
juanpena1 <juanpenlo@unisabana.edu.co>
2024-11-13 10:41:35 -0500

    cambio

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -14,7 +14,11 @@ function LoginPage(){
 
     return(
     <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
-        <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
+        <div className='bg-primary  max-w-md w-full p-10 rounded-md'>
+
+        <div className="flex gap-2 justify-center">
+            <img src="Logo.png" alt="sabanaride" />
+        </div>
 
         {Array.isArray(signinErrors) && signinErrors.map((error, i) => (
           <div key={i} className='bg-red-500 p-2 text-white my-2 rounded text-center'>
@@ -25,16 +29,16 @@ function LoginPage(){
         <form onSubmit={onSubmit} className="space-y-5"
         >
           <input type="email" {...register("email", {required: true})}
-            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2 text-center'
+            className='w-full bg-white text-black px-4 py-2 rounded-md my-2 text-center'
             placeholder='Escribe tu correo'
           />
-          {errors.email && (<p className='text-red-500'>Email is required</p>)}
+          {errors.email && (<p className='text-red-500'>Correo requerido</p>)}
   
           <input type="password" {...register("password", {required: true})}
-            className='w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2 text-center'
+            className='w-full bg-white text-black px-4 py-2 rounded-md my-2 text-center'
             placeholder='Escribe tu contraseña'
           />
-          {errors.password  && (<p className='text-red-500'>Password is required</p>)}
+          {errors.password  && (<p className='text-red-500'>Contraseña requerida</p>)}
   
           <button 
             type="submit" 
